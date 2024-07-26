d0830715035489a5b71c1a2df4edba114c8e8c1f
Fernando <ing.fernandovg@gmail.com>
2024-07-25 20:38:31 -0600

    add create method

--- src/controllers/song.controllers.js
+++ src/controllers/song.controllers.js
@@ -3,11 +3,17 @@ const Song = require('../models/Song');
 
 const getAll = catchError(async(req, res) => {
     const result = await Song.findAll(req.body)
-    return res.json(result)
+    return res.status(200).json(result)
 });
+
+const create = catchError(async(req,res) => {
+    const result = await Song.create(req.body)
+    return res.status(201).json(result)
+})
 
 
 
 module.exports = {
-    getAll
+    getAll,
+    create
 }
--- src/routes/song.router.js
+++ src/routes/song.router.js
@@ -1,9 +1,12 @@
-const { getAll } = require('../controllers/song.controllers');
+const { getAll, create } = require('../controllers/song.controllers');
 const express = require('express');
 const songRouter = express.Router();
 
 songRouter.route("/")
-		.get(getAll)
+    .get(getAll)
+    .post(create)
+
+
 
 
 module.exports = songRouter;
